53241b82d85f9a5b1b4d045a72b7a9930b6d50c7
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-11-10 14:04:14 -0300

    base commit

---
+++ src/main/java/br/com/pedro/__course_challenge_api/Application.java
@@ -0,0 +1,13 @@
+package br.com.pedro.__course_challenge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedro/__course_challenge_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.__course_challenge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
